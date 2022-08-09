3e3d7a0f8e0b03e6a1a79ff880ab3a653ee20e7d
Gabriel Alves <gabrielalves00df@gmail.com>
2022-08-09 00:48:28 -0300

    Customizando o swagger

--- src/main/java/com/gabriel/br/apitraining/controllers/ProductController.java
+++ src/main/java/com/gabriel/br/apitraining/controllers/ProductController.java
@@ -17,8 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gabriel.br.apitraining.data.vo.ProductVO;
 import com.gabriel.br.apitraining.services.ProductServices;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Products", description = "EndPoints for Managing Products")
 public class ProductController {
 	
 	@Autowired
@@ -26,21 +30,25 @@ public class ProductController {
 	
 	@PostMapping(value = "/create",produces = MediaType.APPLICATION_JSON_VALUE,
 				consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Create Product", description = "Create Product", tags = {"Product"})
 	public ProductVO createProduct(@RequestBody ProductVO productVO) {
 		return service.create(productVO);
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Finds All Products", description = "Finds All Products", tags = {"Product"})
 	public List <ProductVO> findAllProducts(){
 		return service.findAll();
 	}
 	
 	@GetMapping(value ="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Find product by ID", description = "Find product by ID", tags = {"Product"})
 	public ProductVO findByIdProduct(@PathVariable(value = "id") long id) {
 		return service.findById(id);
 	}
 	
 	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Delete product by ID", description = "Delete product by ID", tags = {"Product"})
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
@@ -48,6 +56,7 @@ public class ProductController {
 	
 	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
 				consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Update product", description = "Update product", tags = {"Product"})
 	public ProductVO updateProduct(@RequestBody ProductVO productVO) {
 		return service.update(productVO);
 	}
